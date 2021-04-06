c9704380860bf19017d110e373a40d0c8e55135d
paper <donovanreed418@gmail.com>
2021-04-06 04:45:55 -0400

    explained dummied out code

--- SamplePlugin/CordovaNewPlugin/src/windows/CordovaNewPluginProxy.js
+++ SamplePlugin/CordovaNewPlugin/src/windows/CordovaNewPluginProxy.js
@@ -14,7 +14,11 @@ module.exports = {
             errorCallback(error.name + ": " + error.message);
         }
 
-        //pure JS plugin implementation of echo
+        /*
+          pure JS plugin implementation of echo - not used now, but
+          left in as proof of concept. useful when troubleshooting for 
+          determining whether the error lies with the native code or the plugin.
+        */
         /*
         if(!strInput || !strInput.length) {
             errorCallback("Error, something was wrong with the input string. =>" + strInput);
